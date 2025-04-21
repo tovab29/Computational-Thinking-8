cd3ccaaca2f40b0a22da8f340589f52c045d0d83
tovab29 <tovabuerk@seattleacademy.org>
2025-04-21 20:57:43 +0000

    test

---
+++ Projects/codeartintro.py
@@ -0,0 +1,21 @@
+#
+# Setup 
+import turtle 
+#
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("green")
+t.pendown()
+
+for i in range (6)
+    t.forward(60)
+    t.left(60)
+
+
+
+#
+# ENDING
+turtle.exitonclick ()
+#
