a28cbaae5e82239594e7e557756487ed4bb45a61
tovab29 <tovabuerk@seattleacademy.org>
2025-05-01 21:33:33 +0000

    P4.py

--- Projects/P3.py
+++ Projects/P3.py
@@ -11,11 +11,12 @@ for i in range ( 70 ):
     t.color( colors[ i % 3 ] )
     t.forward( 8 )
     t.left( 45 )
+#Change Color
 t.color("cyan")
 for i in range(700):
     t.forward(100)
     t.left(72)
-
+#End
 turtle.exitonclick()
 
 
---
+++ Projects/P4.py/P4.py
@@ -0,0 +1,66 @@
+# Section 1 - Helper functions
+import turtle, time, random
+def set_background(image_filename):
+    screen = turtle.Screen()
+    try:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+    except:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+def create_sprite(image_filename, x=0, y=0):
+    image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite = turtle.Turtle()
+    sprite.shape(image_file)
+    sprite.penup()
+    sprite.goto(x,y)
+    return sprite
+
+
+# Section 2 - Variables
+# TODO - add starting values for all the variables
+x1 = -200
+y1 = 200
+x2 = -200
+y2 = 160
+x3 = -200
+y3 = 140
+x4 =-200
+y4 =120
+
+# Section 3 - Setup
+# TODO - use your own background, and set your four turtles to images of your choice
+set_background("castle")
+t1 = create_sprite("baseball",x1,y1)
+t2 = create_sprite("fish",x2,y2)
+t3 = create_sprite("flower",x3,y3)
+t4 = create_sprite("fox",x4,y4)
+
+
+# # Section 4 - Racing
+# # TODO - set how much each variable changes by and increase the number of repeats to at least 30
+# # TODO - explain here which sprites are faster or slower
+for i in range(100):
+    x1 += random.randint(1,6)
+    x2 +=random.randint(1,6)
+    x3 +=random.randint(1,6)
+    x4 +=random.randint(1,6)
+    t1.goto(x1, y1)
+    t2.goto(x2, y2)
+    t3.goto(x3, y3)
+    t4.goto(x4, y4)
+    time.sleep(0.1)
+
+
+# # Section 5 - Winner
+# # TODO - complete the elif for player 2 winning
+# # TODO - write another elif for player 3 and player 4
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+       print("player 1 wins!")
+elif  x2 >= x1 and x2 >= x3 and x2 >= x4:
+       print("player 2 wins!")
+if x3 >= x1 and x3 >= x2 and x4 >= x4:
+       print("player 3 wins!")
+if x4 >= x1 and x4 >= x2 and x4 >= x3:
+       print("player 3 wins!")
+turtle.exitonclick()
